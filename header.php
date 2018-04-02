094b8d7ac0f69c91b988a75d2114ef0d537ac81d
Nicolas_CELLA <ncella98@gmail.com>
2018-04-02 13:42:16 +0200

    wsw

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ if ($_SESSION['id'] == "" || $_SESSION['login'] == "") {
 				</form>
 				<a href="signin.php"><h2>'.$lang['header_signin'].'</h2></a>
 				<a href="signin.php?el=register"><h2>'.$lang['header_register'].'</h2></a>
-				<h2 class="lang_eng">ENG</h2><h2 class="lang_barre">|</h2><h2 class="lang_fr">FR</h2>
+				<h2 class="lang_eng">EN</h2><h2 class="lang_barre">|</h2><h2 class="lang_fr">FR</h2>
 			</div>
 	</header>
 
@@ -39,13 +39,22 @@ else{
 
 				<a href="setting.php"><img src="assets/icon/settings.svg" alt="setting"/></a>
 				<a href="php/logout.php"><img src="assets/icon/logout.svg" alt="logout"/></a>
-				<h2 class="lang_eng">ENG</h2><h2 class="lang_barre">|</h2><h2 class="lang_fr">FR</h2>
+				<h2 class="lang_eng">EN</h2><h2 class="lang_barre">|</h2><h2 class="lang_fr">FR</h2>
 			</div>
 	</header>
 
 <!-- ******* ALERT ***************** -->
 	<div id="alert" class="alert"></div>';
 }
+
+echo '<!-- ******* NAV MOBILE ***************** -->
+<footer>
+	<a href="chat.php"><img src="assets/icon/chat.svg" alt="chat" /></a>
+	<a href="match.php"><img src="assets/icon/match.svg" alt="match" /></a>
+	<a href="profil.php"><img src="assets/icon/man-user.svg" alt="profil" /></a>
+	<a href="account_setting.php"><img src="assets/icon/settings.svg" alt="setting" /></a>
+	<a href="php/logout.php"><img src="assets/icon/logout.svg" alt="logout" /></a>
+</footer>'
 ?>
 
 
